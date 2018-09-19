1246c912f0f401a742e08c3ae9815eada0b06586
GirugaCode <ryan.dphu.nguyen@gmail.com>
2018-09-18 21:55:32 -0700

    Created a Hero class and Tested values

--- superhero-team-dueler.py
+++ superhero-team-dueler.py
@@ -25,6 +25,23 @@
 
 import random
 
+class Hero:
+    def __init__(self,name):
+        self.abilities = list()
+        self.name = name
+
+    def add_ability(self, ability):
+        # Append ability to self.abilities
+        self.abilities.append(ability)
+
+    def attack(self):
+        # Call the attack method on every ability in our ability list
+        total_attack = 0
+        # Add up and return the total of all attacks
+        for add_attack in self.abilities:
+            total_attack += add_attack.attack()
+        return total_attack
+
 class Ability:
     def __init__(self, name, attack_strength):
         # Initialize starting values
@@ -32,10 +49,24 @@ class Ability:
         self.attack_strength = attack_strength
 
     def attack(self):
-        # Return attack value
+        # Calculate lowest attack value as an integer.
         lowest_attack_value = self.attack_strength // 2
+        # Use random.randint(a, b) to select a random attack value.
         random_attack_value = random.randint(lowest_attack_value, self.attack_strength)
+        # Return attack value between 0 and the full attack
         return random_attack_value
 
     def update_attack(self, attack_strength):
         # Update attack value
+        self.attack_strength = attack_strength
+
+
+if __name__ == "__main__":
+    hero = Hero("Wonder Woman")
+    print(hero.attack())
+    ability = Ability("Divine Speed", 300)
+    hero.add_ability(ability)
+    print(hero.attack())
+    new_ability = Ability("Super Human Strength", 800)
+    hero.add_ability(new_ability)
+    print(hero.attack())
